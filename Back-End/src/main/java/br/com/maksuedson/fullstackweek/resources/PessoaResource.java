3ca7c593bd435015ca4d4e9608c688aaf0a0bfed
Maksuedson <maksuedson@hotmail.com>
2021-03-28 13:09:01 -0300

    Corrigido resource update

--- Back-End/src/main/java/br/com/maksuedson/fullstackweek/resources/PessoaResource.java
+++ Back-End/src/main/java/br/com/maksuedson/fullstackweek/resources/PessoaResource.java
@@ -3,10 +3,12 @@ package br.com.maksuedson.fullstackweek.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import br.com.maksuedson.fullstackweek.repository.PessoaRepository;
 
 @RestController
 @RequestMapping (path = "/pessoas")
+@CrossOrigin(origins = { "http://localhost:3000"})
 public class PessoaResource {
 	
 	@Autowired
@@ -62,7 +65,7 @@ public class PessoaResource {
 //				}).orElse(null);
 //	}
 	
-	@RequestMapping (path = "{codigo}")
+	@PutMapping (path = "{codigo}")
 	public Pessoa atualizar(@PathVariable ("codigo") Long codigo, @RequestBody Pessoa pessoa) {
 		repo.findById(codigo);
 		return repo.save(pessoa);		
